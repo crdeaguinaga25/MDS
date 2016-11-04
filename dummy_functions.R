bc4bd81c54f3acb247f7158eed79f0445d7d4e44
crdeaguinaga25 <crar_25@hotmail.com>
2016-11-04 23:05:48 +0100

    First upload

---
+++ dummy_functions.R
@@ -0,0 +1,20 @@
+###################################
+# Dummy functions for extra points#
+###################################
+
+
+dummy.loop <- function (x){
+  for i in x {
+    print(x [i])
+  } 
+}
+
+dummy.if <- function (x){
+  if(x){
+    print(x)
+  }
+}
+
+dummy.function <- function(){
+  print('dummy')
+}
